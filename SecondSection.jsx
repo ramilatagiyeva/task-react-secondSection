78e0fd87cc6eaa06f3aeed3e1f97b32527f6b592
ramilatagiyeva <tagiyevaramila02@gmail.com>
2024-12-25 16:37:18 +0400

    Add files via upload

---
+++ SecondSection.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './SecondSection.css';
+const SecondSection = () => {
+  return (
+    <section className="second">
+      <div className="container">
+        <div className="second-content">
+        <div className="box-1">
+          <div className="icon">
+            <img src="https://templatemo.com/templates/templatemo_582_tale_seo_agency/assets/images/services-01.jpg" alt="" />
+          </div>
+          <h4>Discover More on current SEO Trends</h4>
+        </div>
+        <div className="box-2">
+          <div className="icon">
+            <img src="https://templatemo.com/templates/templatemo_582_tale_seo_agency/assets/images/services-02.jpg" alt="" />
+          </div>
+          <h4>Real-Time Big Data Analysis</h4>
+        </div>
+        <div className="box-3">
+          <div className="icon">
+            <img src="https://templatemo.com/templates/templatemo_582_tale_seo_agency/assets/images/services-03.jpg" alt="" />
+          </div>
+          <h4>Precise Data Analysis & Prediction</h4>
+        </div>
+        <div className="box-4">
+          <div className="icon">
+            <img src="https://templatemo.com/templates/templatemo_582_tale_seo_agency/assets/images/services-04.jpg" alt="" />
+          </div>
+          <h4>SEO Marketing & Social Media</h4>
+        </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default SecondSection;
